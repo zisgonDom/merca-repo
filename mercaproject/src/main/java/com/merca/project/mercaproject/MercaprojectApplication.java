92647b914076042a613f8f336bd0771dbd60c120
zisgonDom <carlos.domenechantonino@gmail.com>
2023-01-09 16:57:01 +0100

    Creación proyecto

---
+++ mercaproject/src/main/java/com/merca/project/mercaproject/MercaprojectApplication.java
@@ -0,0 +1,13 @@
+package com.merca.project.mercaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercaprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercaprojectApplication.class, args);
+	}
+
+}
---
+++ mercaproject/src/test/java/com/merca/project/mercaproject/MercaprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.merca.project.mercaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercaprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
