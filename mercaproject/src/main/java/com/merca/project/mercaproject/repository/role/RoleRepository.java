4181a306df66419310d729c51cc5736a36bb1b39
zisgon <carlos.domenech@gmail.com>
2023-01-13 16:57:55 +0100

    Eliminada Entidad Role

--- mercaproject/src/main/java/com/merca/project/mercaproject/repository/role/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.merca.project.mercaproject.repository.role;
-
-import com.merca.project.mercaproject.model.Role;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoleRepository extends CrudRepository<Role, Long> {
-}
